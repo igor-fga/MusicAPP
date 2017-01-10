04ae3837d00399f6cf9342ddeeb3932eddb51a3c
Igor Andrade <Igor Andrade>
2017-01-10 00:00:25 -0200

    First commit

---
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.musicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView colors = (TextView) findViewById(R.id.myImageViewText);
+
+        colors.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent colorsIntent = new Intent(MainActivity.this, RecentPlayActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+    }
+}
